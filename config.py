29bdd36981141d3eca373d4c9218126cc1636830
TheWallofSolidarity2026bot <ramiraezgameggf@gmail.com>
2026-05-15 02:00:55 +0100

    Update SERVER_URL for FastAPI webhook

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 import os
 
 # رابط خادم FastAPI (لـ webhook)
-SERVER_URL = os.environ.get("SERVER_URL", "https://thewallofsolidarity20261-bp89.onrender.com")
+SERVER_URL = os.environ.get("SERVER_URL", "https://thewallofsolidarity20261-1.onrender.com")
 
 # سعر البكسل بالدولار
 PIXEL_PRICE = float(os.environ.get("PIXEL_PRICE", "5"))
